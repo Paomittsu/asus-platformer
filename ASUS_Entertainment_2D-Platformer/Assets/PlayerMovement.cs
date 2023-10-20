e9d48486e5c5570efb37311e029a6419912b359d
Paomittsu <philip.figueroa@obf.ateneo.edu>
2023-10-20 19:20:07 +0800

    Jump Cooldown
    - Can no longer spam jump

--- ASUS_Entertainment_2D-Platformer/Assets/PlayerMovement.cs
+++ ASUS_Entertainment_2D-Platformer/Assets/PlayerMovement.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private BoxCollider2D coll;
+    
+    [SerializeField] private LayerMask jumpableGround;
    
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<BoxCollider2D>();
+
     }
 
     // Update is called once per frame
@@ -18,9 +23,14 @@ public class PlayerMovement : MonoBehaviour
         float dirX = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2 (dirX * 7f, rb.velocity.y);
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && isGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, 14f);
         }
+    }
+
+    private bool isGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
     }
 }
