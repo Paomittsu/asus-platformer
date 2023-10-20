a15caa1d34c5a0c0f0753fe3a6fc5ebe0af95adf
unlimitedglassworks <raphael.stacruz@obf.ateneo.edu>
2023-10-21 01:53:03 +0800

    Sprite and animations

--- ASUS_Entertainment_2D-Platformer/Assets/Scripts/PlayerMovement.cs
+++ ASUS_Entertainment_2D-Platformer/Assets/Scripts/PlayerMovement.cs
@@ -6,27 +6,70 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
     private BoxCollider2D coll;
-    
+    private Animator anim;
+    private Vector2 movement;
     [SerializeField] private LayerMask jumpableGround;
+
+    private float dirX;
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float jumpForce = 14f;
    
+    private enum MovementState { idle, walking, jumping, air, landing }
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         coll = GetComponent<BoxCollider2D>();
+        anim = GetComponent<Animator>();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        float dirX = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2 (dirX * 7f, rb.velocity.y);
+        dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2 (dirX * moveSpeed, rb.velocity.y);
 
         if (Input.GetButtonDown("Jump") && isGrounded())
         {
-            rb.velocity = new Vector2(rb.velocity.x, 14f);
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
+
+        UpdateAnimationState();
+    }
+
+    private void UpdateAnimationState()
+    {
+        MovementState state;
+        movement.x = dirX;
+        if (movement != Vector2.zero)
+        {
+            anim.SetFloat("Horizontal", dirX);
+        }
+        if (dirX > 0f)
+        {
+            state = MovementState.walking;
+        }
+        else if (dirX < 0f)
+        {
+            state = MovementState.walking;
+        }
+        else
+        {
+            state = MovementState.idle;
+        }
+
+        if (rb.velocity.y > .1f)
+        {
+            state = MovementState.air;
+        }
+        else if (rb.velocity.y < -.1f)
+        {
+            state = MovementState.landing;
+        }
+
+        anim.SetInteger("state", (int)state);
     }
 
     private bool isGrounded()
