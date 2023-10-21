d0a419a35b0f347ac2c20f93dac4857b94ec0e7f
Paomittsu <philip.figueroa@obf.ateneo.edu>
2023-10-22 05:42:55 +0800

    Added "Win!" screen
    - Planning to add UI button to "restart" game
    - When player completes game, scene freezes and controls are locked

--- ASUS_Entertainment_2D-Platformer/Assets/Scripts/Finish_Line.cs
+++ ASUS_Entertainment_2D-Platformer/Assets/Scripts/Finish_Line.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Finish_Line : MonoBehaviour
 {
 
     private SpriteRenderer sr;
     private bool completed = false;
+    // public TextMeshProUGUI winText;
+    public GameObject winCanvas;
 
 
     // Start is called before the first frame update
@@ -15,6 +18,7 @@ public class Finish_Line : MonoBehaviour
     {
         sr = GetComponent<SpriteRenderer>();
         sr.enabled = false;
+        winCanvas.SetActive(false);
     }
 
     void OnTriggerEnter2D(Collider2D collision)
@@ -25,16 +29,19 @@ public class Finish_Line : MonoBehaviour
             if (SceneManager.GetActiveScene().buildIndex == 2)
             {
                 Debug.Log("GAME COMPLETE");
+                winCanvas.SetActive(true);
+                Time.timeScale = 0;
+                // Disable Movement
             }
             else
             {
-                Invoke("Complete", 2f);
+                Invoke("nextStage", 2f);
             }
         }
         
     }
 
-    private void Complete()
+    private void nextStage()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
