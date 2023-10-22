021dfdde885afc6b1359fba4cc7f58a44e89b425
Paomittsu <philip.figueroa@obf.ateneo.edu>
2023-10-22 23:43:34 +0800

    Scam Platform

---
+++ ASUS_Entertainment_2D-Platformer/Assets/Scripts/Scam_Platform.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scam_Platform : MonoBehaviour
+{
+    public GameObject scam;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("HERE");
+            gameObject.GetComponent<SpriteRenderer>().enabled = false;
+
+        }
+    }
+}
