6f953405b1837fe838b806c87d0d2f6d8a80dd0c
Paomittsu <philip.figueroa@obf.ateneo.edu>
2023-10-23 00:54:13 +0800

    Revert "Merge branch 'main' of https://github.com/Paomittsu/asus-platformer"
    This reverts commit 5c87e614dd67e3756c19a1aa1b84c47340ab9a8b, reversing
    changes made to deeae93b99f6c4f2f383fcf5fe0c4f166b66d6b3.

--- ASUS_Entertainment_2D-Platformer/Assets/Scripts/Scam_Platform.cs
+++ ASUS_Entertainment_2D-Platformer/Assets/Scripts/Scam_Platform.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class Scam_Platform : MonoBehaviour
 {
-<<<<<<< HEAD
-<<<<<<< HEAD
     public Vector2 dir;
-=======
-    public GameObject scam;
->>>>>>> parent of 61354d4 (Scam tile)
-=======
-    public GameObject scam;
->>>>>>> parent of 61354d4 (Scam tile)
     private void OnCollisionEnter2D(Collision2D collision)
     {
         
